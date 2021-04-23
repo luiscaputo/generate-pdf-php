f00c058f4905e452492fa1f105a400460c89daa0
luiscaputo <luiscaputo15@gmail.com>
2021-04-23 11:15:21 +0100

    a

--- pdf.php
+++ pdf.php
@@ -1,5 +1,7 @@
 <?php  
     require_once 'fpdf183/fpdf.php';
+    require('fpdf183/makefont/makefont.php');
+
     if(isset($_POST['g'])){
         $title = filter_input(INPUT_POST, 'title');
         $autor = filter_input(INPUT_POST, 'autor');
@@ -8,19 +10,24 @@
        
             //Gerando
             $pdf = new FPDF();
+
+            //MakeFont('ISO-8859-1');
+                      
             class PDF extends FPDF
             {
             // Page header
             function Header()
             {
+                $title = filter_input(INPUT_POST, 'title');
+                $texto = filter_input(INPUT_POST, 'text');
                 // Logo
-                $this->Image('logo.png',10,6,30);
+                $this->Image('pdf.png',10,6,30);
                 // Arial bold 15
-                $this->SetFont('Arial','B',15);
+                $this->SetFont('Arial','B',15, 'ISO-8859-1');
                 // Move to the right
                 $this->Cell(80);
                 // Title
-                $this->Cell(30,10,'Title',1,0,'C');
+                $this->Cell(30,10,' - '.$title.' - ',100,200,'C');
                 // Line break
                 $this->Ln(20);
             }
@@ -28,12 +35,13 @@
             // Page footer
             function Footer()
             {
+                $autor = filter_input(INPUT_POST, 'autor');
                 // Position at 1.5 cm from bottom
                 $this->SetY(-15);
                 // Arial italic 8
                 $this->SetFont('Arial','I',8);
                 // Page number
-                $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+                $this->Cell(0,10,'created-by-'.$autor.', Page '.$this->PageNo().'/{nb}',0,0,'C');
             }
             }
 
@@ -42,8 +50,7 @@
             $pdf->AliasNbPages();
             $pdf->AddPage();
             $pdf->SetFont('Times','',12);
-            for($i=1;$i<=40;$i++)
-                $pdf->Cell(0,10,'Printing line number '.$i,0,1);
+                $pdf->Cell(0,10, $texto ,0,1);
             $pdf->Output();
             
         #$pdf->Text();
